730c1902524b130222deb288ce42d2004b78a974
dzapeka <douson@gmail.com>
2023-10-13 01:28:13 +0200

    implement feedback widget

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,71 @@
+import { Component } from 'react';
+
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+    positiveFeedback: 0,
+  };
+
+  countTotalFeedback() {
+    this.setState(state => ({
+      total: state.good + state.neutral + state.bad,
+    }));
+  }
+
+  countPositiveFeedbackPercentage() {
+    this.setState(state => ({
+      positiveFeedback: Math.round((state.good / state.total) * 100),
+    }));
+  }
+
+  handleUpdateFeedback = event => {
+    const feedback = event.target.dataset.feedback;
+    this.setState(state => ({
+      [feedback]: state[feedback] + 1,
+    }));
+    this.countTotalFeedback();
+    this.countPositiveFeedbackPercentage();
+  };
+
+  render() {
+    return (
+      <>
+        <h2>Please leave feedback</h2>
+        <button
+          type="button"
+          data-feedback="good"
+          onClick={this.handleUpdateFeedback}
+        >
+          Good
+        </button>
+        <button
+          type="button"
+          data-feedback="neutral"
+          onClick={this.handleUpdateFeedback}
+        >
+          Neutral
+        </button>
+        <button
+          type="button"
+          data-feedback="bad"
+          onClick={this.handleUpdateFeedback}
+        >
+          Bad
+        </button>
+        <h2>Statistics</h2>
+        <section>
+          <p>Good: {this.state.good}</p>
+          <p>Neutral: {this.state.neutral}</p>
+          <p>Bad: {this.state.bad}</p>
+          <p>Total: {this.state.total}</p>
+          <p>Positive feedback: {this.state.positiveFeedback}%</p>
+        </section>
+      </>
+    );
+  }
+}
+
+export default Feedback;
